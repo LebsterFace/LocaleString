74b89effcb9ab8158bff20e8c7a802fc4f654724
LebsterFace <samsalinsky@gmail.com>
2022-06-27 17:11:06 +0100

    Add locale option

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
+// @ts-check
+"use strict";
+
 const result = document.getElementById("result"),
 	optionsContainer = document.getElementById("options"),
 	optionElements = {},
 	codeElement = document.getElementById("codebox"),
 	formatOptions = {};
+
+let chosenLocale = "";
 
 for (const key in possibleProps) {
 	const container = document.createElement("div"),
@@ -22,11 +27,37 @@ for (const key in possibleProps) {
 
 	label.innerText = possibleProps[key].description;
 	select.name = key;
-	select.oninput = setValue;
+	select.addEventListener("input", setValue);
 	container.appendChild(label);
 	container.appendChild(select);
 	container.classList.add("option");
-	optionElements[key] = {select, label};
+	optionElements[key] = { select, label };
+	optionsContainer.appendChild(container);
+}
+
+{
+	const container = document.createElement("div"),
+		label = document.createElement("label"),
+		select = document.createElement("select"),
+		optionTextValues = Object.keys(locales);
+
+	select.appendChild(document.createElement("option"));
+
+	const values = Object.values(locales);
+
+	for (let i = 0; i < values.length; i++) {
+		const option = document.createElement("option");
+		option.value = values[i];
+		option.appendChild(document.createTextNode(optionTextValues[i]));
+		select.appendChild(option);
+	}
+
+	label.innerText = "Locale";
+	select.name = "locale";
+	select.addEventListener("input", setLocale);
+	container.appendChild(label);
+	container.appendChild(select);
+	container.classList.add("option");
 	optionsContainer.appendChild(container);
 }
 
@@ -51,24 +82,38 @@ function setValue(ev) {
 	update();
 }
 
-function getFormattedDate() {
-	return new Date().toLocaleString("en-US", formatOptions);
+function setLocale(ev) {
+	const target = ev.target;
+
+	if (target.value === "") {
+		chosenLocale = "";
+	} else {
+		chosenLocale = target.value;
+	}
+
+	update();
 }
 
+
+function getFormattedDate() {
+	return new Date().toLocaleString(chosenLocale === "" ? [] : chosenLocale, formatOptions);
+}
+
+const getStringifiedLocale = () => chosenLocale === "" ? "[]" : `"${chosenLocale}"`;
 function update() {
 	result.innerText = getFormattedDate();
-	codeElement.textContent = `const formatDate = d => d.toLocaleString("en-US", ${JSON.stringify(formatOptions, undefined, "\t")});\n\nformatDate(new Date()) // ${getFormattedDate()}`;
+	codeElement.textContent = `const formatDate = d => d.toLocaleString(${getStringifiedLocale()}, ${JSON.stringify(formatOptions, undefined, "\t")});\n\nformatDate(new Date()) // ${getFormattedDate()}`;
 }
 
 update();
 
 const scrollDown = document.getElementById("scrollDown");
-scrollDown.addEventListener("click", function() {
-	codeElement.scrollIntoView({behavior: "smooth"});
+scrollDown.addEventListener("click", function () {
+	codeElement.scrollIntoView({ behavior: "smooth" });
 });
 
 const copyButton = document.getElementById("copyButton");
-copyButton.addEventListener("click", function() {
+copyButton.addEventListener("click", function () {
 	const range = document.createRange(),
 		sel = window.getSelection();
 
